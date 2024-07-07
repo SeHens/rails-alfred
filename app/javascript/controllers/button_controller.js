22144c0aaa5c6e4a9ef8487071077175ef53eac0
Hans Langthaler <hansl.mifpt2014@gmail.com>
2024-07-07 18:07:54 +0200

    Final Updates homepage

--- app/javascript/controllers/button_controller.js
+++ app/javascript/controllers/button_controller.js
@@ -2,16 +2,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["button"];
+  static targets = ["button", "text"];
 
   connect() {
     console.log("Hello from button_controller.js");
-    // Listen for the custom event to show the button
-    document.addEventListener('typewriter:finished', this.showButton.bind(this));
+    // Listen for the custom event to show the button and text
+    document.addEventListener('typewriter:finished', this.showElements.bind(this));
   }
 
-  showButton() {
-    console.log("Showing button"); // Debugging log
+  showElements() {
+    console.log("Showing elements"); // Debugging log
+    this.textTarget.style.display = 'block';
     this.buttonTarget.style.display = 'block';
   }
 }
